a14ef3887e7f1732aec477442fdb1d637db8c108
Matheus Rodacki <44122620+matheusrodacki@users.noreply.github.com>
2023-10-04 08:37:13 +0000

    defininsdo assercoes

--- index.js
+++ index.js
@@ -3,14 +3,34 @@ const somaHorasExtras = (salario, valorHorasExtras) =>
 
 const calculaDescontos = (salario, descontos) => salario - descontos;
 
-const teste = (titulo, esperado, retornado) => {
-  if (esperado === retornado) {
-    console.log(`${titulo} passou`);
-  } else {
-    console.error(`${titulo} não passou`);
+const verifiqueSe = (valor) => {
+  const assercoes = {
+    ehExatamenteIgualA(esperado) {
+      if (valor !== esperado) {
+        throw {};
+      }
+    },
+  };
+  return assercoes;
+};
+
+const teste = (titulo, funcaoDeTeste) => {
+  try {
+    funcaoDeTeste();
+    console.log(`${titulo} passou!"`);
+  } catch {
+    console.log(`${titulo} não pasosu!"`);
   }
 };
 
-teste("somaHorasExtras", 2500, somaHorasExtras(2000, 500));
+teste("somaHorasExtras", () => {
+  const esperado = 2500;
+  const retornado = somaHorasExtras(2000, 500);
+  verifiqueSe(retornado).ehExatamenteIgualA(esperado);
+});
 
-teste("calculaDesconto", 2200, calculaDescontos(2500, 300));
+teste("calculaDesconto", () => {
+  const esperado = 2300;
+  const retornado = calculaDescontos(2500, 300);
+  verifiqueSe(retornado).ehExatamenteIgualA(esperado);
+});
